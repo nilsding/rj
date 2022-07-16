b2a5b7f8d7072166676ce371b4e7501c175cd967
Georg Gadinger <nilsding@nilsding.org>
2022-07-16 20:53:36 +0200

    sort the test `require`s

--- test/rq_test.rb
+++ test/rq_test.rb
@@ -3,9 +3,9 @@
 # This runs the entire test suite of rq.
 
 require_relative './basic_object'
-require_relative './usage_from_readme'
-require_relative './debug_flag'
 require_relative './compact'
+require_relative './debug_flag'
 require_relative './internals'
 require_relative './options'
 require_relative './output_format'
+require_relative './usage_from_readme'
