c59e47bcf7e8ba74ef54474123476cbf2c3a0559
Georg Gadinger <nilsding@nilsding.org>
2022-07-22 18:00:43 +0200

    create universal macOS builds

--- mruby_config.rb
+++ mruby_config.rb
@@ -8,4 +8,35 @@ MRuby::Build.new do |conf|
   # C compiler settings
   conf.enable_cxx_abi
   conf.enable_cxx_exception
+
+  if ENV.key?("UNIVERSAL_BUILD")
+    conf.cc.flags << %w(-arch arm64 -arch x86_64)
+    conf.cxx.flags << %w(-arch arm64 -arch x86_64)
+    conf.linker.flags << %w(-arch arm64 -arch x86_64)
+  end
+end
+
+return unless ENV.key?("UNIVERSAL_BUILD")
+
+# Hack to fix the preprocessor command in universal builds:
+
+class MRuby::Command::Compiler
+  # https://github.com/mruby/mruby/blob/3.1.0/lib/mruby/build/command.rb#L87-L100
+  def run(outfile, infile, _defines=[], _include_paths=[], _flags=[])
+    mkdir_p File.dirname(outfile)
+    flags = all_flags(_defines, _include_paths, _flags)
+    if object_ext?(outfile)
+      label = @label
+      opts = compile_options
+    else
+      label = "CPP"
+      opts = preprocess_options
+      flags << " -DMRB_PRESYM_SCANNING"
+      # need to remove the `-arch` flags from the preprocessor command here as clang errors out otherwise:
+      # clang: error: cannot use 'c++-cpp-output' output with multiple -arch options
+      flags = flags.sub('-arch arm64 -arch x86_64', '')
+    end
+    _pp label, infile.relative_path, outfile.relative_path
+    _run opts, flags: flags, infile: filename(infile), outfile: filename(outfile)
+  end
 end
